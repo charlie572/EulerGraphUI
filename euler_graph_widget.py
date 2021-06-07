f8a45c91594e946ffab2725dfffe240712076b4a
charlie572 <barehamcharlie@gmail.com>
2021-06-07 14:52:04 +0100

    Add highlights for hovered nodes
    This commit makes nodes have a highlight around the edges when the mouse is
    hovering over them.

--- euler_graph_widget.py
+++ euler_graph_widget.py
@@ -3,12 +3,20 @@ from PyQt5.QtCore import Qt
 import networkx as nx
 
 
+def point_circle_intersect(point, circle_x, circle_y, circle_diameter):
+    return circle_x - circle_diameter // 2 <= point.x() <= circle_x + circle_diameter // 2 and \
+           circle_y - circle_diameter // 2 <= point.y() <= circle_y + circle_diameter // 2
+
+
 class EulerGraphWidget(QtWidgets.QWidget):
-    def __init__(self, *args, default_node_size=20, default_node_color=Qt.black, **kwargs):
+    def __init__(self, *args, default_node_size=20, default_node_color=Qt.black, hover_colour=Qt.blue, **kwargs):
         super(EulerGraphWidget, self).__init__(*args, **kwargs)
 
         self.default_node_size = default_node_size
         self.default_node_color = default_node_color
+        self.hover_color = hover_colour
+
+        self.setMouseTracking(True)
 
         self.graph = nx.Graph()
 
@@ -17,21 +25,47 @@ class EulerGraphWidget(QtWidgets.QWidget):
             # create node
             node_id = len(self.graph)
             self.graph.add_node(node_id, x=event.x(), y=event.y(),
-                                         size=self.default_node_size,
-                                         color=self.default_node_color)
+                                size=self.default_node_size,
+                                color=self.default_node_color,
+                                hovering=True)
 
             # invoke a paint event
             self.update()
 
+    def mouseMoveEvent(self, event):
+        # check if the mouse is hovering over any nodes
+        for node, data in self.graph.nodes().data():
+            x, y, size, *_ = data.values()
+
+            data["hovering"] = point_circle_intersect(event.pos(), x, y, size)
+
+        self.update()
+
     def paintEvent(self, event):
         painter = QtGui.QPainter(self)
 
+        # pen styles
+        hover_pen = QtGui.QPen()
+        hover_pen.setWidth(2)
+        hover_pen.setColor(self.hover_color)
+
+        normal_pen = QtGui.QPen()
+        normal_pen.setStyle(Qt.NoPen)
+
         # draw nodes
         for node, data in self.graph.nodes().data():
-            x, y, size, color = data.values()
+            x, y, size, color, hovering, *_ = data.values()
 
+            # style the fill
             painter.setBrush(QtGui.QBrush(color, Qt.SolidPattern))
-            painter.drawEllipse(x - size//2, y - size//2, size, size)
+
+            # style the outline
+            if hovering:
+                painter.setPen(hover_pen)
+            else:
+                painter.setPen(normal_pen)
+
+            painter.drawEllipse(x - size // 2, y - size // 2, size, size)
 
         painter.end()
 
