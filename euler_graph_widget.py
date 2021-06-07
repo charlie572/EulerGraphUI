20a19c1e1caede1ed82f6e1db0b38f8880b055f6
charlie572 <barehamcharlie@gmail.com>
2021-06-07 14:17:13 +0100

    Add node creation
    This commit lets the user create nodes using ctrl + right click.

--- euler_graph_widget.py
+++ euler_graph_widget.py
@@ -4,8 +4,36 @@ import networkx as nx
 
 
 class EulerGraphWidget(QtWidgets.QWidget):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, default_node_size=20, default_node_color=Qt.black, **kwargs):
         super(EulerGraphWidget, self).__init__(*args, **kwargs)
+
+        self.default_node_size = default_node_size
+        self.default_node_color = default_node_color
+
+        self.graph = nx.Graph()
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.RightButton and event.modifiers() == Qt.ShiftModifier:
+            # create node
+            node_id = len(self.graph)
+            self.graph.add_node(node_id, x=event.x(), y=event.y(),
+                                         size=self.default_node_size,
+                                         color=self.default_node_color)
+
+            # invoke a paint event
+            self.update()
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+
+        # draw nodes
+        for node, data in self.graph.nodes().data():
+            x, y, size, color = data.values()
+
+            painter.setBrush(QtGui.QBrush(color, Qt.SolidPattern))
+            painter.drawEllipse(x - size//2, y - size//2, size, size)
+
+        painter.end()
 
 
 def main():
