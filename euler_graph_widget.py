013c525f4fbde1ebea88af6c9c2da99db9d3c549
charlie572 <barehamcharlie@gmail.com>
2021-06-08 12:48:06 +0100

    Add edge selection
    This commit allows the user to select edges as well as nodes.

--- euler_graph_widget.py
+++ euler_graph_widget.py
@@ -3,9 +3,42 @@ from PyQt5.QtCore import Qt
 import networkx as nx
 
 
+def between(value, boundary_1, boundary_2):
+    if boundary_1 < boundary_2:
+        return boundary_1 <= value <= boundary_2
+    else:
+        return boundary_2 <= value <= boundary_1
+
+
 def point_circle_intersect(point, circle_x, circle_y, circle_diameter):
     return circle_x - circle_diameter // 2 <= point.x() <= circle_x + circle_diameter // 2 and \
            circle_y - circle_diameter // 2 <= point.y() <= circle_y + circle_diameter // 2
+
+
+def point_line_intersect(point, line_x1, line_y1, line_x2, line_y2, min_distance=5):
+    # the given line
+    m_1 = (line_y1 - line_y2) / (line_x1 - line_x2)
+    c_1 = line_y1 - m_1 * line_x1
+
+    # perpendicular line passing through the point
+    m_2 = -1 / m_1
+    c_2 = point.y() - m_2 * point.x()
+
+    # intersection of these lines
+    intersection_x = (c_2 - c_1) / (m_1 - m_2)
+    intersection_y = m_1 * intersection_x + c_1
+
+    # if the line doesn't intersect the line segment, then check how close the point is to the ends of the line segment
+    if not between(intersection_x, line_x1, line_x2):
+        squared_distance_1 = (point.x() - line_x1) ** 2 + (point.y() - line_y1) ** 2
+        squared_distance_2 = (point.x() - line_x2) ** 2 + (point.y() - line_y2) ** 2
+
+        return squared_distance_1 < min_distance ** 2 or squared_distance_2 < min_distance ** 2
+
+    # squared distance from the point to the line
+    squared_distance = (point.x() - intersection_x) ** 2 + (point.y() - intersection_y) ** 2
+
+    return squared_distance <= min_distance ** 2
 
 
 class EulerGraphWidget(QtWidgets.QWidget):
@@ -29,9 +62,11 @@ class EulerGraphWidget(QtWidgets.QWidget):
         self.mouse_y = None
 
         self.hovered_node = None
+        self.hovered_edge = None
         self.selected_nodes = set()
-
+        self.selected_edges = set()
         self.node_being_selected = None
+        self.edge_being_selected = None
 
         self.moving_nodes = False
 
@@ -48,9 +83,11 @@ class EulerGraphWidget(QtWidgets.QWidget):
                                 color=self.default_node_color,
                                 hovering=True,
                                 selected=False)
+            self.hovered_node = node_id
         elif event.button() == Qt.LeftButton:
             # start selecting the hovered node
             self.node_being_selected = self.hovered_node
+            self.edge_being_selected = self.hovered_edge
 
             if event.modifiers() == Qt.AltModifier:
                 # start drawing an edge if the mouse is hovering over a node
@@ -79,7 +116,7 @@ class EulerGraphWidget(QtWidgets.QWidget):
 
                     if end_node is not None and not self.graph.has_edge(start_node, end_node) and \
                             end_node != start_node:
-                        self.graph.add_edge(start_node, end_node)
+                        self.graph.add_edge(start_node, end_node, hovering=False, selected=False)
 
                     self.edge_start_node = None
                     self.drawing_edge = False
@@ -88,8 +125,13 @@ class EulerGraphWidget(QtWidgets.QWidget):
                 # select this node
                 self.selected_nodes.add(self.node_being_selected)
                 self.graph.nodes[self.node_being_selected]["selected"] = True
+            elif self.edge_being_selected is not None and self.edge_being_selected == self.hovered_edge:
+                # select this edge
+                self.selected_edges.add(self.edge_being_selected)
+                self.graph.edges[self.edge_being_selected]["selected"] = True
 
             self.node_being_selected = None
+            self.edge_being_selected = None
 
         self.update()
 
@@ -110,6 +152,21 @@ class EulerGraphWidget(QtWidgets.QWidget):
                 break
         else:
             self.hovered_node = None
+
+            # check if the mouse is hovering over any edges
+            for start_node, end_node in self.graph.edges():
+                line_x1 = self.graph.nodes[start_node]["x"]
+                line_y1 = self.graph.nodes[start_node]["y"]
+                line_x2 = self.graph.nodes[end_node]["x"]
+                line_y2 = self.graph.nodes[end_node]["y"]
+
+                hovering = point_line_intersect(event.pos(), line_x1, line_y1, line_x2, line_y2)
+                self.graph.edges[start_node, end_node]["hovering"] = hovering
+                if hovering:
+                    self.hovered_edge = (start_node, end_node)
+                    break
+            else:
+                self.hovered_edge = None
 
         # move nodes
         if self.moving_nodes:
@@ -154,12 +211,19 @@ class EulerGraphWidget(QtWidgets.QWidget):
             painter.drawEllipse(x - size // 2, y - size // 2, size, size)
 
         # draw edges
-        painter.setPen(normal_pen)
         for start_node, end_node in self.graph.edges():
             start_x = self.graph.nodes[start_node]["x"]
             start_y = self.graph.nodes[start_node]["y"]
             end_x = self.graph.nodes[end_node]["x"]
             end_y = self.graph.nodes[end_node]["y"]
+
+            # style the line
+            if self.graph.edges[start_node, end_node]["hovering"]:
+                painter.setPen(hover_pen)
+            elif self.graph.edges[start_node, end_node]["selected"]:
+                painter.setPen(select_pen)
+            else:
+                painter.setPen(normal_pen)
 
             painter.drawLine(start_x, start_y, end_x, end_y)
 
@@ -175,7 +239,11 @@ class EulerGraphWidget(QtWidgets.QWidget):
         for node in self.selected_nodes:
             self.graph.nodes[node]["selected"] = False
 
+        for edge in self.selected_edges:
+            self.graph.edges[edge]["selected"] = False
+
         self.selected_nodes.clear()
+        self.selected_edges.clear()
 
 
 def main():
